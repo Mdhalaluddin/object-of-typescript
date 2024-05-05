91e752531c632e28558a58fee8920ae5f25f0b5a
MD Halal Uddin <mdhelaluddin505@gmail.com>
2024-05-05 18:32:35 +0600

    generic tuple added

--- module2/2.3.ts
+++ module2/2.3.ts
@@ -1,23 +1,30 @@
 {
-//
-// generic types
+    //
+    // generic types
 
-// const rollNumber: number[]  = [2, 4, 5, 6];
+    // const rollNumber: number[]  = [2, 4, 5, 6];
 
-const rollNumber : Array<number> = [2,3,4];
-
-
-// const mentors: string[] = ["Mr", 'X', 'y'];
-
-const mentors: Array<string> = ["Mr", 'X', 'y'];
-
-// const boolArray: boolean[] = [true, false, true];
-
-const boolArray: Array<boolean> = [true, false, true]
+    const rollNumber: Array<number> = [2, 3, 4];
 
 
+    // const mentors: string[] = ["Mr", 'X', 'y'];
 
+    const mentors: Array<string> = ["Mr", 'X', 'y'];
 
+    // const boolArray: boolean[] = [true, false, true];
 
-//
+    const boolArray: Array<boolean> = [true, false, true];
+
+    // generic tuple
+
+    type GenericTuple<x, y> = [x, y]
+
+    const manush: GenericTuple<string, string> = ['Mr. x', 'Mr. Y']
+
+    const UserWithID : GenericTuple<number, { name: string, email: string }> = [1234, { name: 'Halal', email: 'helal@gamil.com' }]
 }
+
+
+
+
+    //
