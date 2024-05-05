16637c356c745d94270c50a05d5bdb31aa8c53c7
MD Halal Uddin <mdhelaluddin505@gmail.com>
2024-05-05 16:09:34 +0600

    generic types

---
+++ module2/2.3.ts
@@ -0,0 +1,23 @@
+{
+//
+// generic types
+
+// const rollNumber: number[]  = [2, 4, 5, 6];
+
+const rollNumber : Array<number> = [2,3,4];
+
+
+// const mentors: string[] = ["Mr", 'X', 'y'];
+
+const mentors: Array<string> = ["Mr", 'X', 'y'];
+
+// const boolArray: boolean[] = [true, false, true];
+
+const boolArray: Array<boolean> = [true, false, true]
+
+
+
+
+
+//
+}
